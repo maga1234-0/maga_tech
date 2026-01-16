13bd3388ad52849bd014d27bb41c08005ab6e8c8
Maga Aubin <magaaubin3@gmail.com>
2026-01-16 20:51:46 +0000

    for the email get the user directly this email once he clicks on. aubinm

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -26,7 +26,7 @@ export const bio =
   "I am AUBIN MAGA, a software engineer graduated from KIGALI INDEPENDENT UNIVERSITY (ULK). As a passionate software developer, I thrive on building innovative solutions that leverage cutting-edge technology. My journey in the tech world is driven by a relentless curiosity and a desire to solve complex problems. From developing scalable web applications to exploring the frontiers of artificial intelligence, I am committed to creating software that is not only functional but also elegant and user-centric. I believe in the power of code to transform ideas into reality and am always eager to embrace new challenges and technologies.";
 
 export const contact = {
-  email: 'your-email@magatech.com',
+  email: 'aubinmaga@gmail.com',
   whatsapp: '+243980453932',
   github: 'https://github.com/maga1234-0/protefolio',
 };
